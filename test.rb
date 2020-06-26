1849d420fd8a386629b18a3d024b80f652361e90
Bryan Oyloe <boyloe@gmail.com>
2020-06-26 15:25:33 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -3,5 +3,8 @@ programmer_hash =
         :known_for => "Unix",
         :languages => ["C"]
       }
+      binding.pry
       
-      programmer_hash[:dennis_ritchie][:known_for] => 
+      programmer_hash[:dennis_ritchie][:known_for] => {rogrammer_hash[:dennis_ritchie][:known_for], "Assembly"
+      binding.pry
+      
